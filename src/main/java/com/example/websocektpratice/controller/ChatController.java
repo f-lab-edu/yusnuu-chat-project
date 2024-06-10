42fb2127ce4c38745f3d0e47a0e417dbc45e1ab3
unknown <yunsuu>
2024-06-10 17:03:44 +0900

    chatConatroller 구현

---
+++ src/main/java/com/example/websocektpratice/controller/ChatController.java
@@ -0,0 +1,35 @@
+package com.example.websocektpratice.controller;
+
+
+import com.example.websocektpratice.Service.ChatService;
+import com.example.websocektpratice.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ChatController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    private final ChatService chatService;
+    public ChatController(SimpMessagingTemplate simpMessagingTemplate, ChatService chatService) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.chatService = chatService;
+    }
+
+    @GetMapping("/chat")
+    public String chatPageRouter() {
+        return "chat";
+    }
+    @MessageMapping("/chat.sendMessage")
+    public void sendMessage(@Payload ChatMessage chatMessage) throws Exception {
+        chatService.sendMessageFromClientToRBMQ(chatMessage);
+        chatService.sendMessageFromRBMQToClient(chatMessage.getRoomId());
+
+
+    }
+}
